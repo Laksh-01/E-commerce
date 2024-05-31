195958d6c8a00b7e2ad6ceac1c3f027593ba31e5
Laksh-01 <lakshsachdeva01@gmail.com>
2024-05-31 11:42:56 +0530

    added pagination

--- backend/utils/apiFeatures.js
+++ backend/utils/apiFeatures.js
@@ -36,8 +36,14 @@ class ApiFeatures{
         
     }
 
-    pagination(){
+    pagination(resultPerPage){
+        const currentPage = Number(this.queryStr.page) || 1;
         
+        const skip = resultPerPage * (currentPage - 1);
+
+        this.query = this.query.limit(resultPerPage).skip(skip);
+        
+        return this;
     }
 
 

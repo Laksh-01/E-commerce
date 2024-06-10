7d341aa1e6b2fa3a5ed14f7d06577d65fcb0495d
Laksh-01 <lakshsachdeva01@gmail.com>
2024-06-11 03:46:28 +0530

    done slight changes regarding env

--- backend/config/database.js
+++
@@ -1,9 +0,0 @@
-const mongoose = require("mongoose")
-
-const connectDataBase = ()=>{
-    mongoose.connect(process.env.DB_URI).then((data)=>{
-        console.log(`MonogDb connected with server : ${data.connection.host}`)
-    })
-}
-
-module.exports = connectDataBase

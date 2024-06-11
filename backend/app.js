f505666eaad111eb66a1d43a281613e4dd69a4a8
Laksh-01 <lakshsachdeva01@gmail.com>
2024-06-12 04:50:24 +0530

    created order api

--- backend/app.js
+++ backend/app.js
@@ -8,9 +8,11 @@ app.use(cookieParser());
 
 const product = require("./routes/productRoute.js")
 const user = require("./routes/userRoute.js")
+const order = require('./routes/orderRoute.js')
 
 app.use("/api/v1" , product);
 app.use("/api/v1" , user);
+app.use("/api/v1" , order);
 app.use(errorMiddleware);
 
 module.exports = app
